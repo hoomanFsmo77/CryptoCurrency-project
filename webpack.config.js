5591d7b0811d435adcfab5a74d42186b8f7bc87b
hooman_77 <hoomanmousavi@gmail.com>
2022-09-30 01:49:51 +0330

    add webpack, its configuration file and plugins

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path=require('path')
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports={
+    entry: 'src/javascript/main.js',
+    output: {
+        filename: "bundle[contenthash].js",
+        path: path.resolve(__dirname,'./dist'),
+        assetModuleFilename: 'assets/[name][ext]'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(png|svg|jpe?g)/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    plugins: [new MiniCssExtractPlugin({
+        filename:'css/bundle[contenthash].css'
+    }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        })
+    ],
+}
