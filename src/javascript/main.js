25f6823ee81d03b17456938f7f75635b0ca1957d
hooman_77 <hoomanmousavi@gmail.com>
2022-09-30 23:25:58 +0330

    add button and utilities  styles

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,7 +2,7 @@ import Alpine from "alpinejs";
 import '../images/title_logo_sm.png'
 import '../scss/main.scss';
 import '../scss/responsive.scss';
-
+/////////////////////////////////////////////////////////////////////////////////////////
 window.Alpine=Alpine
 Alpine.start()
 
