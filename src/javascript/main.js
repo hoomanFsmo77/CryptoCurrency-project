a285773eae0b978457eecb3530b252f64e628729
hooman_77 <hoomanmousavi@gmail.com>
2022-09-30 16:27:48 +0330

    add new scss files and add alpine js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,4 +1,6 @@
-import Box from "./components/Box/Box.js";
+import Alpine from "alpinejs";
 import '../scss/main.scss';
+import '../scss/responsive.scss';
+window.Alpine=Alpine
+Alpine.start()
 
-window.customElements.define('box',Box)
