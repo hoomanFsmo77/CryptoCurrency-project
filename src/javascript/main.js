58a60aa44f4c2f17e3824ebb0dccf855706054df
hooman_77 <hoomanmousavi@gmail.com>
2022-10-01 16:36:08 +0330

    add persist pluging and configure local storage for dark switcher

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,8 +1,9 @@
 import Alpine from "alpinejs";
+import persist from '@alpinejs/persist';
 import '../images/title_logo_sm.png'
 import '../scss/main.scss';
 import '../scss/responsive.scss';
 /////////////////////////////////////////////////////////////////////////////////////////
 window.Alpine=Alpine
+Alpine.plugin(persist)
 Alpine.start()
-
