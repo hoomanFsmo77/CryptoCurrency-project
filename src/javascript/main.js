81d7cde51b9760c906798307634d3ed0287c2462
hooman_77 <hoomanmousavi@gmail.com>
2022-10-01 23:14:55 +0330

    main navbar created, canvas created, search bar created

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,6 +1,7 @@
 import Alpine from "alpinejs";
 import persist from '@alpinejs/persist';
 import '../images/title_logo_sm.png'
+import '../images/title_logo.png';
 import '../scss/main.scss';
 import '../scss/responsive.scss';
 /////////////////////////////////////////////////////////////////////////////////////////
